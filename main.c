a8662f437c9d05d1b2733383a0bb89f45df41685
Iago Caran Aquino <iago.caran@gmail.com>
2021-04-25 23:49:57 +0200

    Add corrections to compile on Windows systems and improve code formatting

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+#ifdef WIN32
+    #include <windows.h>
+    #define sleep(X)(Sleep(X))
+    #define random()(rand())
+#else
+    #include <unistd.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -11,9 +19,10 @@
 //   char** ingredients;
 // } Receipt;
 
-typedef enum {WAITING, STARTING, PREPARING, COOKING, DELIVERED} meal_status;
+typedef enum { WAITING, STARTING, PREPARING, COOKING, DELIVERED } meal_status;
 
-const char* possible_status[] = {"WAITING", "STARTING", "PREPARING", "COOKING", "DELIVERED"};
+const char * possible_status[] = { "WAITING", "STARTING", "PREPARING", "COOKING", "DELIVERED" };
+
 typedef struct {
   int number;
   int chef; // who is responsible for this order
@@ -26,34 +35,35 @@ Order orders[ORDERS];
 sem_t *sem_order;
 // sem_t sem_oven; // to be used in the future
 
-int get_next_order(){
-    
-    for(int i=0;i<ORDERS;i++)
+int get_next_order() {
+    for(int i = 0; i < ORDERS; i++)
         if(orders[i].status == WAITING)
             return i;
     
     return -1;
 }
 
-int get_ingredients(){
-    sleep(random()%3); 
+int get_ingredients() {
+    sleep(random()%3);
     return 1;
 }
 
-int cut_ingredients(){
+int cut_ingredients() {
     sleep(random()%3);
     return 1; 
 }
 
-int cook_meal(){
-    sleep(random()%3); 
+int cook_meal() {
+    sleep(random()%3);
     return 1;
 }
-int deliver_meal(){
-    sleep(random()%3); 
+
+int deliver_meal() {
+    sleep(random()%3);
     return 1;
 }
-void show_status(){
+
+void show_status() {
     printf("\n-------------------------------------------------\n");
     for(int i=0;i<ORDERS;i++){
         printf("order id: %d; ",orders[i].number);
@@ -62,35 +72,35 @@ void show_status(){
     }
     printf("\n-------------------------------------------------\n");
 }
-void* t_chef(void* v){
+
+void* t_chef(void* v) {
     int id = *(int*) v;
     
     typedef int (*function)(void);
-    function functions[4] = {&get_ingredients, &cut_ingredients, &cook_meal, &deliver_meal}; 
+    function functions[4] = { &get_ingredients, &cut_ingredients, &cook_meal, &deliver_meal };
 
     while(1) {
-    sem_wait(sem_order);
-    int next_order = get_next_order();
-    
-    if(next_order < 0) {
-        sem_post(sem_order);
-        return NULL;
-    }
-    
-    orders[next_order].chef = id;
-    orders[next_order].status = STARTING;
-    sem_post(sem_order);
-    
-    for(int i=0;i<4;i++) {
         sem_wait(sem_order);
-        functions[i]();
-        orders[next_order].status = i+1;
-        sem_post(sem_order);
-    }
+        int next_order = get_next_order();
 
-    sleep(random()%3);
-  }
-  return NULL;
+        if(next_order < 0) {
+            sem_post(sem_order);
+            return NULL;
+        }
+
+        orders[next_order].chef = id;
+        orders[next_order].status = STARTING;
+        sem_post(sem_order);
+
+        for(int i = 0; i < 4; i++) {
+            sem_wait(sem_order);
+            functions[i]();
+            orders[next_order].status = i + 1;
+            sem_post(sem_order);
+        }
+
+        sleep(random() % 3);
+    }
 }
 
 void* t_status(){
@@ -103,12 +113,17 @@ void* t_status(){
 }
 
 int main() {
-    pthread_t thr_chefs[CHEFS],thr_status;
+    pthread_t thr_chefs[CHEFS], thr_status;
     int id_chef[CHEFS];
 
-    //initializing semaphore - only one thread can access it at the time. MacOS doesn't support sem_init, that's why I'm using sem_open
-    sem_order = sem_open("sem_order",O_CREAT | O_EXCL, 0644,1);
-    
+    // initializing semaphore - only one thread can access it at the time. MacOS doesn't support sem_init, that's why I'm using sem_open
+#ifdef WIN32
+    sem_order = malloc(sizeof(sem_t));
+    sem_init(sem_order, 0, 1);
+#else
+    sem_order = sem_open("sem_order", O_CREAT | O_EXCL, 0644, 1);
+#endif
+
     sem_wait(sem_order);
     
     // creating orders queue
@@ -126,7 +141,8 @@ int main() {
 
     for (int i = 0; i < CHEFS; i++) 
         pthread_join(thr_chefs[i], NULL);
-    
+
+    pthread_cancel(thr_status);
     pthread_join(thr_status, NULL);
     
     return EXIT_SUCCESS;
